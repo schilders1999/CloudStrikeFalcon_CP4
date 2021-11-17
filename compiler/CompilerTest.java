f784f357183daa92f2ff4fa160471fe568dc47bc
schilders1999 <schilders@bellarmine.edu>
2021-11-16 19:37:40 -0500

    11/16/2021 - Spencer Childers - Testing push

--- compiler/CompilerTest.java
+++ compiler/CompilerTest.java
@@ -1,4 +1,4 @@
-package knightcodecompiler;
+package compiler;
 /**
  * This class encapsulates a basic grammar test.
  */
@@ -21,17 +21,47 @@ public class CompilerTest{
         KnightCodeLexer lexer;
         CommonTokenStream tokens;
         KnightCodeParser parser;
+        
+        
+        
+        
+        
+        String file;
+	String output;
+	if(args.length == 2){
+	
+		file = args[0];
+		output = args[1];
+		
+	} else if(args.length == 1){ 
+	
+		file = args[0];
+		output = "output/output1";
+	
+	} else {
+	
+		file = "tests/program1.kc";
+		output = "output/output1";
+	
+	}
 
         try{
-            input = CharStreams.fromFileName(args[0]);  //get the input
+            input = CharStreams.fromFileName(file);  //get the input
             lexer = new KnightCodeLexer(input); //create the lexer
             tokens = new CommonTokenStream(lexer); //create the token stream
             parser = new KnightCodeParser(tokens); //create the parser
        
             ParseTree tree = parser.file();  //set the start location of the parser
+            
+            
+            
+            
+	    myListener listener = new myListener(output, false);
+	    ParseTreeWalker walker = new ParseTreeWalker();
+	    walker.walk(listener, tree);
              
             
-            Trees.inspect(tree, parser);
+            //Trees.inspect(tree, parser);
             
             //System.out.println(tree.toStringTree(parser));
         
